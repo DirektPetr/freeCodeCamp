f19976fb71f9112363a62f99a4669c6b325a74dc
Petr <gruzvash@gmail.com>
2024-06-10 00:46:19 +0300

    commit 25 step methods array

---
+++ 25_step.js
@@ -0,0 +1,8 @@
+let character = 'Hello';
+let count = 8;
+let rows = ['Naomi', 'Quincy', 'CamperChan'];
+let pushed = rows.push('freeCodeCamp');
+console.log(pushed); //  4
+let popped = rows.pop();
+console.log(popped); //  freeCodeCamp
+console.log(rows); //  ['Naomi', 'Quincy', 'CamperChan']
